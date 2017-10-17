dfb933130bc01ca42fba5f28198caf17be50ef9b
Devaraj Phukan <devarajphukan@Devarajs-MacBook-Pro.local>
2017-10-17 17:22:35 +0530

    clean stemmer

--- Revised_lda.py
+++ Revised_lda.py
@@ -1,4 +1,4 @@
-import re, nltk        
+import re, nltk
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
 from gensim import corpora, models
@@ -13,15 +13,13 @@ def stem_tokens(tokens, stemmer):
 	return stemmed
 
 def tokenize(text):
-	
+
 	text = re.sub("[^a-zA-Z]", " ", text) # Removing numbers and punctuation
 	text = re.sub(" +"," ", text) # Removing extra white space
 	text = re.sub("\\b[a-zA-Z0-9]{10,100}\\b"," ",text) # Removing very long words above 10 characters
 	text = re.sub("\\b[a-zA-Z0-9]{0,1}\\b"," ",text) # Removing single characters (e.g k, K)
 	tokens = nltk.word_tokenize(text.strip())
 	tokens = nltk.pos_tag(tokens)
-	# Uncomment next line to use stemmer
-	# tokens = stem_tokens(tokens, stemmer)
 	return tokens
 
 stopset = stopwords.words('english')
@@ -30,14 +28,14 @@ for i in freq_words :
     stopset.append(i)
 
 def analyze(fileObj,Uname) :
-	
+
 	text_corpus = []
 	for doc in fileObj :
 		temp_doc = tokenize(doc.strip())
 		current_doc = []
 		for word in range(len(temp_doc)) :
 			if temp_doc[word][0] not in stopset and temp_doc[word][1] == 'NN':
-				current_doc.append(temp_doc[word][0])        
+				current_doc.append(temp_doc[word][0])
 
 		text_corpus.append(current_doc)
 
